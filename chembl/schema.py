02e0f04b4f4f4942d066d56f254725bdfacd95c0
zhonghua-wang <zhonghua.wang@outlook.com>
2017-03-16 00:02:53 +0800

    add query helper

--- chembl/schema.py
+++ chembl/schema.py
@@ -1,27 +1,55 @@
 from . import models as chembl_models
 from graphene import AbstractType, Node
-from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django import DjangoObjectType
+import django
+from share.graphql_helper import query_register
 
-from share.graphql_helper import node_register
+
+class Query(AbstractType):
+    pass
 
 
+@query_register(Query)
 class ActionTypeNode(DjangoObjectType):
     class Meta:
         model = chembl_models.ActionType
         interfaces = (Node,)
 
 
+@query_register(Query)
 class ActivitiesNode(DjangoObjectType):
     class Meta:
         model = chembl_models.Activities
         interfaces = (Node,)
 
 
-@node_register([
-    ActivitiesNode,
-    ActionTypeNode
-])
-class Query(AbstractType):
-    all_activities = Node.Field(ActivitiesNode)
+@query_register(Query)
+class ActivityStdsLookupNode(DjangoObjectType):
+    class Meta:
+        model = chembl_models.ActivityStdsLookup
+        interfaces = (Node,)
 
+
+@query_register(Query)
+class AssayParametersNode(DjangoObjectType):
+    class Meta:
+        model = chembl_models.AssayParameters
+        interfaces = (Node,)
+
+
+@query_register(Query)
+class AssayTypeNode(DjangoObjectType):
+    class Meta:
+        model = chembl_models.ActionType
+        interfaces = (Node,)
+
+
+@query_register(Query)
+class AssaysNode(DjangoObjectType):
+    class Meta:
+        model = chembl_models.Assays
+        interfaces = (Node,)
+
+for attr in dir(chembl_models):
+    if isinstance(chembl_models, django.db.models.ModelBase):
+        pass # todo to be continue
--- share/graphql_helper.py
+++ share/graphql_helper.py
@@ -6,8 +6,11 @@ def query_register(query):
     def _wrapper(node):
         node_name = node.__name__.lower()[:-4]
         print node_name
+        meta = query._meta
+        meta.fields[node_name] = Node.Field(node)
+        meta.fields['all_' + node_name] = DjangoFilterConnectionField(node)
         query._meta.fields[node_name] = Node.Field(node)
-        setattr(query, 'all_' + node_name, DjangoFilterConnectionField(node))
+        setattr(query, '_meta', meta)
         return node
 
     return _wrapper
@@ -17,8 +20,10 @@ def node_register(node_list):
     def _wrapper(query):
         for node in node_list:
             node_name = node.__name__.lower()[:-4]
-            setattr(query, node_name, Node.Field(node))
-            setattr(query, 'all_' + node_name, DjangoFilterConnectionField(node))
+            meta = query._meta
+            meta.fields[node_name] = Node.Field(node)
+            meta.fields['all_'+node_name] = DjangoFilterConnectionField(node)
+            setattr(query, '_meta', meta)
         return query
 
     return _wrapper
