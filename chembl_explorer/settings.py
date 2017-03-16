e51c57799f3359d7d8ee463727c53c088bdb4329
zhonghua-wang <zhonghua.wang@outlook.com>
2017-03-17 00:02:27 +0800

    with 403 error

--- chembl_explorer/settings.py
+++ chembl_explorer/settings.py
@@ -42,15 +42,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
-]
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    ]
 
 ROOT_URLCONF = 'chembl_explorer.urls'
 
@@ -137,4 +137,6 @@ GRAPHENE = {
 }
 
 # Cors 
+
 CORS_ORIGIN_ALLOW_ALL = True
+
