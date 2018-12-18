e637ab727ba65172d692745ec76587832ca5cecb
zhonghua wang <zhonghua.wang@outlook.com>
2018-12-18 15:38:21 +0800

    use smiles substructure search for django-rdkit molfield

--- django_rdkit_BK/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'django_rdkit.apps.DjangoRDKitConfig'
--- django_rdkit_BK/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoRDKitConfig(AppConfig):
-    name = 'django_rdkit'
-    verbose_name = 'Django-RDkit'
-
-
